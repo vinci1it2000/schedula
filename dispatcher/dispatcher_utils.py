c8df20b15f607f6319c36d03efe76dd661c95d4a
Vincenzo Arcidiacono <vincenzo.arcidiacono@polito.it>
2015-05-26 21:15:32 +0200

    dispatcher_utils: Fix bypass + Add summation and grouping.

--- dispatcher/dispatcher_utils.py
+++ dispatcher/dispatcher_utils.py
@@ -13,4 +13,12 @@ def combine(*args):
 
 
 def bypass(*args):
-    return args
+    return args if len(args) > 1 else args[0]
+
+
+def summation(*args):
+    return sum(args)
+
+
+def grouping(*args):
+    return tuple(args)
