348d7d7230c4bf46db2374c70cad1dca70dc44e4
Vincenzo Arcidiacono <vinci1it2000@gmail.com>
2015-06-24 13:23:01 +0200

    compas: multiple fix.

---
+++ tests/dispatcher/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+#
+# Copyright 2014 European Commission (JRC);
+# Licensed under the EUPL (the 'Licence');
+# You may not use this work except in compliance with the Licence.
+# You may obtain a copy of the Licence at: http://ec.europa.eu/idabc/eupl
+
+__author__ = 'Vincenzo Arcidiacono'
