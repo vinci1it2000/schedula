ff72a6182aaf28f75a9529d42dfe04a52c94868a
Vincenzo Arcidiacono <vincenzo.arcidiacono@polito.it>
2015-07-14 08:09:43 +0200

    docs constants.

--- compas/functions/constants.py
+++ compas/functions/constants.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 #
@@ -15,26 +14,26 @@ __author__ = 'Vincenzo Arcidiacono'
 
 import sys
 
-#: Minimum vehicle velocity
+#: Minimum vehicle velocity.
 VEL_EPS = 1 + sys.float_info.epsilon
 
-#: Minimum vehicle acceleration
+#: Minimum vehicle acceleration.
 ACC_EPS = 0.1 + sys.float_info.epsilon
 
-#: Infinite value
+#: Infinite value.
 INF = 10000.0
 
-#: Minimum gear
+#: Minimum gear.
 MIN_GEAR = 0
 
-#: Maximum Dt in speed shift equation
+#: Maximum Dt in speed shift equation.
 MAX_DT_SHIFT = 3.0
 
-#: Maximum m in speed shift equation
+#: Maximum m in speed shift equation.
 MAX_M_SHIFT = 1
 
-#: Minimum vehicle engine speed
+#: Minimum vehicle engine speed.
 MIN_ENGINE_SPEED = 10.0
 
-#: Time window applied to the filters
+#: Time window applied to the filters.
 TIME_WINDOW = 4.0
