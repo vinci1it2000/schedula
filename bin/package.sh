4bc6165f4bfa209513e59b7857508539b858866a
Kostis Anagnostopoulos at STUW025 <ankostis@gmail.com>
2015-09-30 13:34:35 +0200

    build: Script to package docs.

--- bin/package.sh
+++ bin/package.sh
@@ -1,2 +1,16 @@
+my_dir=`dirname "$0"`
+cd $my_dir/..
+
 rm -rf build/* dist/*
-python setup.py build bdist_wheel sdist 
+python setup.py build bdist_wheel sdist
+
+## Build docs
+gitver="$(git describe --tags)"
+zipfolder="co2mpas-doc-$gitver"
+docdir="build/doc/$zipfolder"
+mkdir -p build/doc
+
+cp -lr doc/_build/html "$docdir"
+pushd build/doc
+zip -r "../../dist/$zipfolder.zip" "$zipfolder"
+
