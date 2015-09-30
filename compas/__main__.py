19cb1be1a5e9b5c96fc00604b3a6aaad82e94f42
ankostis on tokoti <ankostis@gmail.com>
2015-09-30 22:37:21 +0200

    Impl 'samples' cmd.
    + Add dummy sample-files.

--- compas/__main__.py
+++ compas/__main__.py
@@ -3,6 +3,7 @@ Predict NEDC CO2 emissions from WLTP cycles.
 
 Usage:
     co2mpas [options] [-I <folder>  -O <folder>]
+    co2mpas samples [-f | --force] <folder>
     co2mpas template [-f | --force] <excel-file> ...
     co2mpas --help
     co2mpas --version
@@ -14,10 +15,9 @@ Usage:
 --more-output                    Output also per-vehicle output-files.
 --no-warn-gui                    Does not pause batch-run to report inconsistencies.
 --plot-workflow                  Show workflow in browser, after run finished.
--f --force                       Overwrite template excel-file if it exists.
+-f --force                       Overwrite template/sample excel-file(s).
+
 """
-# [-f | --force]
-# -f --force                       Create template even if file already exists.
 import sys
 import os
 import shutil
@@ -35,8 +35,10 @@ def _get_input_template_fpath():
                                            'input_template.xlsx')
 
 
-def _create_input_template(fpaths, force=False):
-    for fpath in fpaths:
+def _create_input_template(opts):
+    dst_fpaths = opts['<excel-file>']
+    force = opts['--force']
+    for fpath in dst_fpaths:
         fpath = os.path.abspath(fpath)
         if not fpath.endswith('.xlsx'):
             fpath = '%s.xlsx' % fpath
@@ -45,9 +47,38 @@ def _create_input_template(fpaths, force=False):
         if os.path.isdir(fpath):
             exit("Expecting a file-name instead of directory '%s'!" % fpath)
 
-        print("Creating co2mpas INPUT template-file '%s'..." % fpath,
+        print("Creating co2mpas TEMPLATE input-file '%s'..." % fpath,
               file=sys.stderr)
         shutil.copy(_get_input_template_fpath(), fpath)
+
+
+def _get_sample_files():
+    samples = pkg_resources.resource_listdir(__name__,  # @UndefinedVariable
+                                             'samples')
+    return [pkg_resources.resource_filename(__name__,  # @UndefinedVariable
+                                            os.path.join('samples',
+                                                         f))
+            for f in samples]
+
+
+def _copy_sample_files(opts):
+    dst_folder = opts['<folder>']
+    force = opts['--force']
+    dst_folder = os.path.abspath(dst_folder)
+    if not os.path.exists(dst_folder):
+        exit("Destination-<folder> '%s' does not exist!" % dst_folder)
+    if not os.path.isdir(dst_folder):
+        exit("Destination '%s' is not a <folder>!" % dst_folder)
+
+    for src_fpath in _get_sample_files():
+        dst_fpath = os.path.join(dst_folder, os.path.basename(src_fpath))
+        if os.path.exists(dst_fpath) and not force:
+            print("Skipping file '%s', already exists! Use '-f' to overwrite it." %
+                 dst_fpath, file=sys.stderr)
+        else:
+            print("Creating co2mpas SAMPLE input-file '%s'..." % dst_fpath,
+                  file=sys.stderr)
+            shutil.copy(src_fpath, dst_fpath)
 
 
 def _prompt_folder(folder_name, folder):
@@ -86,7 +117,9 @@ def main(*args):
                   argv=args or sys.argv[1:],
                   version='%s %s' % (proj_name, proj_ver))
     if opts['template']:
-        _create_input_template(opts['<excel-file>'], opts['--force'])
+        _create_input_template(opts)
+    elif opts['samples']:
+        _copy_sample_files(opts)
     else:
         _run_simulation(opts)
 
