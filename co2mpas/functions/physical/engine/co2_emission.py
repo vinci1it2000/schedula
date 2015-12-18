79dcab5fe6fce18ff878348640ae759b5c9b944b
Kostis Anagnostopoulos <ankostis@gmail.com>
2015-12-18 18:29:28 +0100

    theta, #94: FIX norm-theta TC & improv algo.

--- co2mpas/functions/physical/engine/co2_emission.py
+++ co2mpas/functions/physical/engine/co2_emission.py
@@ -35,12 +35,12 @@ def calculate_normalized_engine_coolant_temperatures(
     :rtype: numpy.array
     """
 
-    i = np.argmax(engine_coolant_temperatures >= temperature_target)
-    if temperature_target > engine_coolant_temperatures.max():
-        i = len(engine_coolant_temperatures)
-    T = np.ones_like(engine_coolant_temperatures, dtype=float)
-    T[:i] = engine_coolant_temperatures[:i] + 273.0
-    T[:i] /= temperature_target + 273.0
+    first_hot_i = np.argmax(engine_coolant_temperatures >= temperature_target)
+    T = engine_coolant_temperatures + 273.0
+    ## Only flatten-out hot-part if `max-theta` is above `trg`.
+    if first_hot_i > 0:
+        T[first_hot_i:] = temperature_target + 273.0
+    T /= temperature_target + 273.0
 
     return T
 
