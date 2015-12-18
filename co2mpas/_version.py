c906089a70f9d12d2815f0baf716a07cfc5f5e6c
Kostis Anagnostopoulos <ankostis@gmail.com>
2015-12-18 19:07:46 +0100

    Bump v1.1.0.dev2 <-- v1.1.0.dev1.

--- co2mpas/_version.py
+++ co2mpas/_version.py
@@ -7,11 +7,11 @@
 
 
 #: Authoritative project's PEP 440 version.
-__version__ = version = "1.1.0.dev1"  # Also update README.rst & CHANGES.rst.
+__version__ = version = "1.1.0.dev2"  # Also update README.rst & CHANGES.rst.
 
 #: Input/Output file's version.
 __file_version__ = "2"
 
 # Please UPDATE TIMESTAMP WHEN BUMPING VERSIONS AND BEFORE RELASE.
 #: Release date.
-__updated__ = "2015-12-16 13:45:36"
+__updated__ = "2015-12-18 18:59:25"
