e094209a624b5bc82829b387ee621ac08f4442ed
Vincenzo Arcidiacono <vinci1it2000@gmail.com>
2016-02-15 15:15:32 +0100

    synchronization, #144, #145, #148: Add main to synchronization in order run as script.

--- co2mpas/functions/io/synchronization.py
+++ co2mpas/functions/io/synchronization.py
@@ -43,27 +43,43 @@ def synchronization(reference, *data, x_id='times', y_id='velocities'):
     :rtype: list[dict]
     """
 
+    _spline = partial(Spline, k=1, ext=3)
     dx = np.median(np.diff(reference[x_id])) / 10
     m, M = min(reference[x_id]), max(reference[x_id])
     for d in data:
         m, M = min(min(d[x_id]), m), max(max(d[x_id]), M)
 
     X = np.linspace(m, M, int((M - m) / dx))
-
-    splines = []
-    for d in data:
-        s = {k: Spline(d[x_id], v, k=1, ext=3) for k, v in d.items() if k != x_id}
-        splines.append(s)
-
     Y = Spline(reference[x_id], reference[y_id], k=1, ext=3)(X)
 
-    shifts = [compute_shift(Y, s[y_id](X)) * dx for s in splines]
+    x = reference[x_id]
+    for d in data:
+        s = {k: Spline(d[x_id], v, k=1, ext=3) if k != x_id else lambda *a: x
+             for k, v in d.items()}
 
-    X = reference[x_id]
-    res = []
-    for s, dx in zip(splines, shifts):
-        d = {k: v(X + dx) for k, v in s.items()}
-        d[x_id] = X
-        res.append(d)
+        shift = compute_shift(Y, s[y_id](X)) * dx
 
-    return res
+        x_shifted = x + shift
+
+        yield shift, {k: v(x_shifted) for k, v in s.items()}
+
+
+if __name__ == '__main__':
+    import pandas as pd
+    import easygui as eu
+    file_path = eu.fileopenbox('choose', default='*.xlsx')
+
+    excel = pd.ExcelFile(file_path)
+
+    d = [pd.read_excel(excel, sn, header=1) for sn in excel.sheet_names]
+
+    res = [(0, d[0])] + [(s, pd.DataFrame(r)) for s, r in synchronization(*d)]
+
+    file_path = eu.filesavebox('save', default='*.xlsx')
+    writer = pd.ExcelWriter(file_path)
+
+    for (s, df), sn in zip(res, excel.sheet_names):
+        print('%s is shifted by: %.3f' % (sn, s))
+        df.to_excel(writer, sn)
+
+    writer.save()
