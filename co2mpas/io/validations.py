0c3201604211c1566c7c8152adec0ece64ee6401
arcidvi <vinci1it2000@gmail.com>
2016-11-10 17:26:26 +0100

    vva, #287: FIX vva check.

--- co2mpas/io/validations.py
+++ co2mpas/io/validations.py
@@ -57,8 +57,7 @@ def hard_validation(data, usage, stage, cycle, *args):
             _check_ki_factor,
             _check_prediction_gears_not_mt,
             _check_lean_burn_tech,
-            _check_vva,
-            _check_scr
+            _check_vva
         )
         for check in checks:
             c = check(data, usage, stage, cycle, *args)
@@ -223,13 +222,14 @@ def _check_vva(data, usage, stage, cycle, *args):
     s = ('engine_has_variable_valve_actuation', 'ignition_type')
     it = _get_engine_model(s[1:]).dispatch(data, outputs=s[1:]).get(s[1], None)
     from ..model.physical.defaults import dfl
-    has_lb = data.get(s[0], dfl.values.has_lean_burn)
-    if has_lb and it not in ('positive', None):
+    has_vva = data.get(s[0], dfl.values.engine_has_variable_valve_actuation)
+    if has_vva and it not in ('positive', None):
         msg = "`engine_has_variable_valve_actuation` cannot be enable with " \
               "`ignition_type = '%s'`." \
               "Hence, set `engine_has_variable_valve_actuation = False` or " \
               "set `ignition_type = 'positive'`!" % it
         return s, msg
+
 
 def _check_scr(data, usage, stage, cycle, *args):
     s = ('has_selective_catalytic_reduction', 'ignition_type')
