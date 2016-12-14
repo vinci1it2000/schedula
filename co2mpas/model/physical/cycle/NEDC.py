cf1b2ae3db3bc554ba1d0c5b337a4f3a4ad00bf3
arcidvi <vinci1it2000@gmail.com>
2016-12-14 11:50:27 +0100

    cycle: Simplify code of nedc_gears and nedc_velocities functions.

--- co2mpas/model/physical/cycle/NEDC.py
+++ co2mpas/model/physical/cycle/NEDC.py
@@ -14,6 +14,7 @@ import scipy.interpolate as sci_itp
 import co2mpas.dispatcher.utils as dsp_utl
 import co2mpas.dispatcher as dsp
 import numpy as np
+import functools
 
 
 # noinspection PyUnusedLocal
@@ -74,54 +75,45 @@ def nedc_velocities(times, gear_box_type):
     parts = {
         'manual':
             {
-                'part one': [
-                    [0, 0], [11, 0], [15, 15], [23, 15], [25, 10], [28, 0],
-                    [49, 0], [54, 15], [56, 15], [61, 32], [85, 32], [93, 10],
-                    [96, 0], [117, 0], [122, 15], [124, 15], [133, 35],
-                    [135, 35], [143, 50], [155, 50], [163, 35], [176, 35],
-                    [178, 35], [185, 10], [188, 0], [195, 0]
-                ],
-                'part two': [
-                    [0, 0], [20, 0], [25, 15], [27, 15], [36, 35], [38, 35],
-                    [46, 50], [48, 50], [61, 70], [111, 70], [119, 50],
-                    [188, 50], [201, 70], [251, 70], [286, 100], [316, 100],
-                    [336, 120], [346, 120], [362, 80], [370, 50], [380, 0],
-                    [400, 0]
-                ]
+                0: (
+                    (0, 0), (11, 0), (15, 15), (23, 15), (25, 10), (28, 0),
+                    (49, 0), (54, 15), (56, 15), (61, 32), (85, 32), (93, 10),
+                    (96, 0), (117, 0), (122, 15), (124, 15), (133, 35),
+                    (135, 35), (143, 50), (155, 50), (163, 35), (176, 35),
+                    (178, 35), (185, 10), (188, 0), (195, 0)
+                ),
+                4: (
+                    (0, 0), (20, 0), (25, 15), (27, 15), (36, 35), (38, 35),
+                    (46, 50), (48, 50), (61, 70), (111, 70), (119, 50),
+                    (188, 50), (201, 70), (251, 70), (286, 100), (316, 100),
+                    (336, 120), (346, 120), (362, 80), (370, 50), (380, 0),
+                    (400, 0)
+                )
             },
         'automatic':
             {
-                'part one': [
-                    [0, 0], [11, 0], [15, 15], [23, 15], [25, 10], [28, 0],
-                    [49, 0], [61, 32], [85, 32], [93, 10], [96, 0], [117, 0],
-                    [143, 50], [155, 50], [163, 35], [176, 35], [178, 35],
-                    [185, 10], [188, 0], [195, 0]
-                ],
-                'part two': [
-                    [0, 0], [20, 0], [61, 70], [111, 70], [119, 50], [188, 50],
-                    [201, 70], [251, 70], [286, 100], [316, 100], [336, 120],
-                    [346, 120], [362, 80], [370, 50], [380, 0], [400, 0]]
+                0: (
+                    (0, 0), (11, 0), (15, 15), (23, 15), (25, 10), (28, 0),
+                    (49, 0), (61, 32), (85, 32), (93, 10), (96, 0), (117, 0),
+                    (143, 50), (155, 50), (163, 35), (176, 35), (178, 35),
+                    (185, 10), (188, 0), (195, 0)
+                ),
+                4: (
+                    (0, 0), (20, 0), (61, 70), (111, 70), (119, 50), (188, 50),
+                    (201, 70), (251, 70), (286, 100), (316, 100), (336, 120),
+                    (346, 120), (362, 80), (370, 50), (380, 0), (400, 0)
+                )
             }
     }
 
+    for k, v in parts.items():
+        v[1] = v[2] = v[3] = v[0]
+
     parts['cvt'] = parts['automatic']
 
-    parts = parts[gear_box_type]
+    tv = _repeat_parts(times, parts[gear_box_type])
 
-    t, v = zip(*parts['part one'])
-
-    _t, velocities = _repeat_part_one(t, v)
-
-    t, v = zip(*parts['part two'])
-
-    _t.extend(np.asarray(t) + _t[-1])
-    velocities.extend(v)
-
-    n = int(np.ceil(times[-1] / _t[-1]))
-
-    v = np.interp(times, np.tile(_t, (n,)), np.tile(velocities, (n,)))
-
-    return v
+    return np.interp(times, *tv.T)
 
 
 def nedc_gears(times, max_gear, k1=1, k2=2, k5=2):
@@ -152,48 +144,46 @@ def nedc_gears(times, max_gear, k1=1, k2=2, k5=2):
         Gear vector [-].
     :rtype: numpy.array
     """
-    eps = 0.01
+    eps, parts = 0.01, {}
     # part one
-    t, s = zip(*[
-        [0, 0], [6, 0], [6, k1], [11, k1], [11, 1], [25, 1], [25, k1], [28, k1],
-        [28, 0], [44, 0], [44, k1], [49, k1], [49, 1], [55 - eps, 1],
-        [55 - eps, 2], [93, 2], [93, k2], [96, k2], [96, 0], [112, 0],
-        [112, k1], [117, k1], [117, 1], [123 - eps, 1], [123 - eps, 2],
-        [134 - eps, 2], [134 - eps, 3], [177 - eps, 3], [177 - eps, 2],
-        [185, 2], [185, k2], [188, k2], [188, 0], [195, 0]
-    ])
-
-    _t, shifting = _repeat_part_one(t, s)
+    parts[0] = parts[1] = parts[2] = parts[3] = (
+        (0, 0), (6, 0), (6, k1), (11, k1), (11, 1), (25, 1), (25, k1), (28, k1),
+        (28, 0), (44, 0), (44, k1), (49, k1), (49, 1), (55 - eps, 1),
+        (55 - eps, 2), (93, 2), (93, k2), (96, k2), (96, 0), (112, 0),
+        (112, k1), (117, k1), (117, 1), (123 - eps, 1), (123 - eps, 2),
+        (134 - eps, 2), (134 - eps, 3), (177 - eps, 3), (177 - eps, 2),
+        (185, 2), (185, k2), (188, k2), (188, 0), (195, 0)
+    )
 
     # part two
-    t, s = zip(*[
-        [0, k1], [20, k1], [20, 1], [26 - eps, 1], [26 - eps, 2], [37 - eps, 2],
-        [37 - eps, 3], [47 - eps, 3], [47 - eps, 4], [61, 4], [61, 5], [115, 5],
-        [115, 4], [201, 4], [201, 5], [286, 5], [286, max_gear],
-        [370, max_gear], [370, k5], [380, k5], [380, 0], [400, 0]
-    ])
+    parts[4] = (
+        (0, k1), (20, k1), (20, 1), (26 - eps, 1), (26 - eps, 2), (37 - eps, 2),
+        (37 - eps, 3), (47 - eps, 3), (47 - eps, 4), (61, 4), (61, 5), (115, 5),
+        (115, 4), (201, 4), (201, 5), (286, 5), (286, max_gear),
+        (370, max_gear), (370, k5), (380, k5), (380, 0), (400, 0)
+    )
 
-    _t.extend(np.asarray(t) + _t[-1])
-    shifting.extend(s)
-
-    s = sci_itp.interp1d(
-        _t, shifting, kind='nearest', assume_sorted=True
-    )(times)
-
+    tg = _repeat_parts(times, parts).T
+    s = sci_itp.interp1d(*tg, kind='nearest', assume_sorted=True)(times)
     s[s > max_gear] = max_gear
 
     return s
 
 
-def _repeat_part_one(times, values):
-    t, v = [times[0]], [values[0]]
-    times = np.asarray(times[1:])
-    values = values[1:]
-    for i in range(4):
-        t.extend(times + t[-1])
-        v.extend(values)
+def _repeat_parts(times, parts):
+    it = [v[1] for v in sorted(parts.items())]
 
-    return t, v
+    def _func(x, y):
+        y = np.array(y)
+        y[:, 0] += x[-1][-1][0]
+        return x + (y,)
+
+    tv = np.concatenate(functools.reduce(_func, it[1:], (it[0],)))
+    n = int(np.ceil(times[-1] / tv[-1][0])) - 1
+    if n > 0:
+        tv = np.concatenate(functools.reduce(_func, (tv,) * n, (tv,)))
+
+    return tv
 
 
 def nedc_cycle():
