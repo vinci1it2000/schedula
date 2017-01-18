39d03f809d4edc31a5b2883a7c75400aba9cbdff
vincenzoarcidiacono <vinci1it2000@gmail.com>
2017-01-18 10:00:07 +0100

    feat(version): Add version module.

--- setup.py
+++ setup.py
@@ -7,17 +7,36 @@
 # You may obtain a copy of the Licence at: http://ec.europa.eu/idabc/eupl
 
 from setuptools import setup, find_packages
+import io
+import os.path as osp
+
+name = 'schedula'
+mydir = osp.dirname(__file__)
+
+# Version-trick to have version-info in a single place,
+# taken from: http://stackoverflow.com/questions/2058802/how-can-i-get-the-version-defined-in-setup-py-setuptools-in-my-package
+##
+def read_project_version():
+    fglobals = {}
+    with io.open(osp.join(
+            mydir, name, '_version.py'), encoding='UTF-8') as fd:
+        exec(fd.read(), fglobals)  # To read __version__
+    return fglobals['__version__']
+
+proj_ver = read_project_version()
+download_url = 'https://github.com/vinci1it2000/%s/releases/tag/v%s' % (name, proj_ver)
 
 setup(
-    name='schedula',
-    version='0.0.1',
+    name=name,
+    version=proj_ver,
     packages=find_packages(exclude=[
         'test', 'test.*',
         'doc', 'doc.*',
         'appveyor', 'requirements'
     ]),
     url='',
-    license='',
+    download_url=download_url,
+    license='EUPL 1.1+',
     author='Vincenzo Arcidiacono',
     author_email='vinci1it2000@gmail.com',
     description='A dispatch function calls.',
@@ -57,7 +76,9 @@ setup(
         'flask',
         'pycel'
     ],
-    dependency_links=['https://github.com/vinci1it2000/pycel/tarball/master#egg=pycel-0.0.1'],
+    dependency_links=[
+        'https://github.com/vinci1it2000/pycel/tarball/master#egg=pycel-0.0.1'
+    ],
     test_suite='nose.collector',
     setup_requires=['nose>=1.0'],
 
