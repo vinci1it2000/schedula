8ed7b00a25633fade3e1c7d6d6e0958d1302bd49
arcidvi <vinci1it2000@gmail.com>
2017-02-06 18:08:24 +0100

    feat(exl): Drop exl module because of formulas.

--- setup.py
+++ setup.py
@@ -111,13 +111,9 @@ setup(
         'regex',
         'openpyxl>=2.4.0',
         'flask',
-        'pycel',
         'sphinx',
         'decorator',
         'requests'
-    ],
-    dependency_links=[
-        'https://github.com/vinci1it2000/pycel/tarball/master#egg=pycel-0.0.1'
     ],
     test_suite='nose.collector',
     setup_requires=['nose>=1.0'],
