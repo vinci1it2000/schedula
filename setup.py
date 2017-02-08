f09062acb1b6ca000efab5aacf2307fec04a9589
arcidvi <vinci1it2000@gmail.com>
2017-02-08 10:59:12 +0100

    fix(setup): Avoid setup failure due to get_long_description.

--- setup.py
+++ setup.py
@@ -28,15 +28,11 @@ def read_project_version():
 
 
 def get_long_description(cleanup=True):
-    # noinspection PyBroadException
-    try:
-        from sphinx.application import Sphinx
-        from sphinx.util.osutil import abspath
-        import tempfile
-        import shutil
-        from sphinxcontrib.writers.rst import RstTranslator
-    except:
-        return ''
+    from sphinx.application import Sphinx
+    from sphinx.util.osutil import abspath
+    import tempfile
+    import shutil
+    from sphinxcontrib.writers.rst import RstTranslator
     outdir = tempfile.mkdtemp(prefix='setup-', dir='.')
     exclude_patterns = os.listdir(mydir or '.')
     exclude_patterns.remove('pypi.rst')
@@ -60,61 +56,68 @@ proj_ver = read_project_version()
 url = 'https://github.com/vinci1it2000/%s' % name
 download_url = '%s/tarball/v%s' % (url, proj_ver)
 
-setup(
-    name=name,
-    version=proj_ver,
-    packages=find_packages(exclude=[
-        'test', 'test.*',
-        'doc', 'doc.*',
-        'appveyor', 'requirements'
-    ]),
-    url=url,
-    download_url=download_url,
-    license='EUPL 1.1+',
-    author='Vincenzo Arcidiacono',
-    author_email='vinci1it2000@gmail.com',
-    description='Produce a plan that dispatches calls based on a graph of '
-                'functions, satisfying data dependencies.',
-    long_description=get_long_description(),
-    keywords=[
-        "python", "utility", "library", "data", "processing",
-        "calculation", "dependencies", "resolution", "scientific",
-        "engineering", "dispatch", "scheduling", "simulink", "graphviz",
-    ],
-    classifiers=[
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: Implementation :: CPython",
-        "Development Status :: 3 - Alpha",
-        'Natural Language :: English',
-        "Intended Audience :: Developers",
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: European Union Public Licence 1.1 "
-        "(EUPL 1.1)",
-        "Operating System :: MacOS :: MacOS X",
-        "Operating System :: Microsoft :: Windows",
-        "Operating System :: POSIX",
-        "Operating System :: Unix",
-        "Operating System :: OS Independent",
-        "Topic :: Scientific/Engineering",
-        "Topic :: Scientific/Engineering :: Information Analysis",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "Topic :: Utilities",
-    ],
-    install_requires=[
-        'networkx',
-        'dill',
-        'graphviz',
-        'docopt',
-        'regex',
-        'openpyxl>=2.4.0',
-        'flask',
-        'sphinx',
-        'decorator',
-        'requests'
-    ],
-    test_suite='nose.collector',
-    setup_requires=['nose>=1.0'],
-)
+if __name__ == '__main__':
+    # noinspection PyBroadException
+    try:
+        long_description = get_long_description()
+    except:
+        long_description = ''
+
+    setup(
+        name=name,
+        version=proj_ver,
+        packages=find_packages(exclude=[
+            'test', 'test.*',
+            'doc', 'doc.*',
+            'appveyor', 'requirements'
+        ]),
+        url=url,
+        download_url=download_url,
+        license='EUPL 1.1+',
+        author='Vincenzo Arcidiacono',
+        author_email='vinci1it2000@gmail.com',
+        description='Produce a plan that dispatches calls based on a graph of '
+                    'functions, satisfying data dependencies.',
+        long_description=long_description,
+        keywords=[
+            "python", "utility", "library", "data", "processing",
+            "calculation", "dependencies", "resolution", "scientific",
+            "engineering", "dispatch", "scheduling", "simulink", "graphviz",
+        ],
+        classifiers=[
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 3.4",
+            "Programming Language :: Python :: 3.5",
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: Implementation :: CPython",
+            "Development Status :: 3 - Alpha",
+            'Natural Language :: English',
+            "Intended Audience :: Developers",
+            "Intended Audience :: Science/Research",
+            "License :: OSI Approved :: European Union Public Licence 1.1 "
+            "(EUPL 1.1)",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+            "Operating System :: POSIX",
+            "Operating System :: Unix",
+            "Operating System :: OS Independent",
+            "Topic :: Scientific/Engineering",
+            "Topic :: Scientific/Engineering :: Information Analysis",
+            "Topic :: Software Development :: Libraries :: Python Modules",
+            "Topic :: Utilities",
+        ],
+        install_requires=[
+            'networkx',
+            'dill',
+            'graphviz',
+            'docopt',
+            'regex',
+            'openpyxl>=2.4.0',
+            'flask',
+            'sphinx',
+            'decorator',
+            'requests'
+        ],
+        test_suite='nose.collector',
+        setup_requires=['nose>=1.0'],
+    )
