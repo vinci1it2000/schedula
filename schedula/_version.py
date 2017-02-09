dda749026872ace210cf26b24ffffcb056540871
Vincenzo Arcidiacono <vinci1it2000@gmail.com>
2017-02-09 15:25:35 +0100

    chore(ver): bump 0.1.8 -->  0.1.9

--- schedula/_version.py
+++ schedula/_version.py
@@ -7,8 +7,8 @@
 
 
 #: Authoritative project's PEP 440 version.
-__version__ = version = "0.1.8"  # Also update README.rst
+__version__ = version = "0.1.9"  # Also update README.rst
 
 # Please UPDATE TIMESTAMP WHEN BUMPING VERSIONS AND BEFORE RELEASE.
 #: Release date.
-__updated__ = "2017-02-09 03:00:00"
+__updated__ = "2017-02-09 16:00:00"
