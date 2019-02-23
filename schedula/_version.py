6b97829f6af893b63f1ccad58c637f485ebeee4a
vincenzoarcidiacono <vinci1it2000@gmail.com>
2019-02-23 16:00:50 +0100

    chore(ver): bump 0.3.1 --> 0.3.2.

--- schedula/_version.py
+++ schedula/_version.py
@@ -1,6 +1,6 @@
 # !/usr/bin/env python
 #
-# Copyright 2015-2018, Vincenzo Arcidiacono;
+# Copyright 2015-2019, Vincenzo Arcidiacono;
 # Licensed under the EUPL (the 'Licence');
 # You may not use this work except in compliance with the Licence.
 # You may obtain a copy of the Licence at: http://ec.europa.eu/idabc/eupl
@@ -9,11 +9,11 @@ __all__ = ['__version__', '__updated__', '__title__', '__author__',
            '__license__', '__copyright__']
 
 #: Authoritative project's PEP 440 version.
-__version__ = version = "0.3.1"  # Also update README.rst
+__version__ = version = "0.3.2"  # Also update README.rst
 
 # Please UPDATE TIMESTAMP WHEN BUMPING VERSIONS AND BEFORE RELEASE.
 #: Release date.
-__updated__ = "2018-12-10 15:00:00"
+__updated__ = "2019-02-23 16:00:00"
 
 __title__ = 'schedula'
 
@@ -21,4 +21,4 @@ __author__ = 'Vincenzo Arcidiacono <vinci1it2000@gmail.com>'
 
 __license__ = 'EUPL, see LICENSE.txt'
 
-__copyright__ = 'Copyright 2015-2018, Vincenzo Arcidiacono'
+__copyright__ = 'Copyright 2015-2019, Vincenzo Arcidiacono'
