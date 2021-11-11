aea2a02be23ba85b7966e6e890a9c5908b463c39
vincenzoarcidiacono <vinci1it2000@gmail.com>
2021-11-11 15:26:39 +0100

    fix(bin): Correct script.

--- bin/clean.sh
+++ bin/clean.sh
@@ -2,6 +2,6 @@
 cd "$(dirname "$0")" && cd ..
 rm -vrf ./py ./mpy ./dist ./build
 rm -vrf ./*.pyc ./*.tgz ./*.egg-info ./MANIFEST ./*.py.exp ./*.py.out
-if [ $1 = "--all" ]; then
+if [[ $1 = "--all" ]]; then
   rm -rf ./micropython
 fi
