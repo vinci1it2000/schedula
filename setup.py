ed86047a4d98f9dc1c9b53c43341168c4248e6c2
vincenzoarcidiacono <vinci1it2000@gmail.com>
2022-01-13 11:45:07 +0100

    feat(setup): Add missing dev requirement.

--- setup.py
+++ setup.py
@@ -95,7 +95,7 @@ if __name__ == '__main__':
     extras['dev'] = extras['all'] + [
         'wheel', 'sphinx>4', 'gitchangelog', 'mako', 'sphinx_rtd_theme',
         'setuptools>=36.0.1', 'sphinxcontrib-restbuilder', 'nose', 'coveralls',
-        'requests', 'readthedocs-sphinx-ext'
+        'requests', 'readthedocs-sphinx-ext', 'twine'
     ]
 
     setup(
