c3f8f93785e318c9dee9d1cfc153cc4b00927d6d
vincenzoarcidiacono <vinci1it2000@gmail.com>
2022-10-25 15:59:41 +0200

    fix(form): Correct plotting behaviour.

--- schedula/utils/form/react/src/components/plot.js
+++ schedula/utils/form/react/src/components/plot.js
@@ -1,8 +1,8 @@
 import Plot from 'react-plotly.js';
-
+import cloneDeep from 'lodash/cloneDeep'
 
 export default function _plot(props) {
     let {context, children, ...kw} = props
-    return <Plot {...Object.assign({}, context.props.formData, kw)}/>
+    return <Plot {...Object.assign(cloneDeep(context.props.formData), kw)}/>
 
 }
