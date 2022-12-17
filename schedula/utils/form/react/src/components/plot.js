6d6c1868282def13fc7033f421ae795259e75183
vincenzoarcidiacono <vinci1it2000@gmail.com>
2022-12-17 11:41:31 +0100

    fix(form): Correct bug when plot is empty.

--- schedula/utils/form/react/src/components/plot.js
+++ schedula/utils/form/react/src/components/plot.js
@@ -4,6 +4,5 @@ import Plot from 'react-plotly.js'
 
 export default function _plot(props) {
     let {context, children, ...kw} = props
-    return <Plot {...Object.assign(cloneDeep(context.props.formData), kw)}/>
-
+    return <Plot {...Object.assign(cloneDeep(context.props.formData || {}), kw)}/>
 }
