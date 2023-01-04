37657100db080911f37a9f26e02e96d8519aa108
vincenzoarcidiacono <vinci1it2000@gmail.com>
2023-01-04 14:22:47 +0100

    refact(core): Update copyright.

--- tests/docs/util.py
+++ tests/docs/util.py
@@ -1,11 +1,11 @@
-# -*- coding: utf-8 -*-
-"""
-    Sphinx test suite utilities
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+#
+# Copyright 2015-2023, Vincenzo Arcidiacono;
+# Licensed under the EUPL (the 'Licence');
+# You may not use this work except in compliance with the Licence.
+# You may obtain a copy of the Licence at: http://ec.europa.eu/idabc/eupl
 
-    :copyright: Copyright 2007-2015 by the Sphinx team, see AUTHORS.
-    :license: BSD, see LICENSE for details.
-"""
 
 import os
 import sys
