11b8b7f9d2c59f52766f24199b965dddfa139a4e
vincenzoarcidiacono <vinci1it2000@gmail.com>
2023-11-08 17:49:38 +0100

    feat(form): Remove unuseful log.

--- schedula/utils/form/react/src/core/components/Element/index.js
+++ schedula/utils/form/react/src/core/components/Element/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 
 export default function Element({children, render, type, ...props}) {
-    console.log(children, render, type, props)
     return React.createElement(type, props, children)
 }
