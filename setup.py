2afd83faf393cb5b3b78d2f0e32e1fb074b4d36b
vincenzoarcidiacono <vinci1it2000@gmail.com>
2023-11-10 09:56:40 +0100

    fix(setup): Update form requirements.

--- setup.py
+++ setup.py
@@ -82,17 +82,17 @@ if __name__ == '__main__':
     extras = {
         'io': ['dill!=0.2.7'],
         'web': ['requests', 'regex', 'flask'],
-        'form': [
-            'requests', 'regex', 'flask', 'itsdangerous', 'flask_sqlalchemy',
-            'flask_babel', 'flask_wtf', 'flask_principal', 'flask_security',
-            'sqlalchemy', 'flask_mail', 'rst2txt', 'docutils'
-        ],
         'parallel': ['multiprocess'],
         'plot': [
             'requests', 'graphviz>=0.17', 'regex', 'flask', 'Pygments',
             'jinja2', 'docutils'
         ]
     }
+    extras['form'] = extras['web'] + [
+        'itsdangerous', 'rst2txt', 'flask-sqlalchemy', 'sqlalchemy', 'docutils',
+        'flask-babel', 'flask-wtf', 'flask-principal', 'flask-security-too',
+        'flask-mail'
+    ]
     extras['sphinx'] = ['sphinx>=7'] + extras['plot']
     extras['all'] = sorted(functools.reduce(set.union, extras.values(), set()))
     extras['dev'] = extras['all'] + [
