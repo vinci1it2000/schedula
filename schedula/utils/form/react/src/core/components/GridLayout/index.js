b900f155c66fc3aedb70433b3c4fb5015f33f663
vincenzoarcidiacono <vinci1it2000@gmail.com>
2024-03-28 22:01:11 +0100

    feat(form): Add `ResponsiveGridLayout` component.

---
+++ schedula/utils/form/react/src/core/components/GridLayout/index.js
@@ -0,0 +1,107 @@
+import {Component} from "react";
+import {Responsive, WidthProvider} from "react-grid-layout";
+import './GridLayout.css'
+
+const ResponsiveGridLayout = WidthProvider(Responsive);
+
+class Layout extends Component {
+    static defaultProps = {
+        className: "layout",
+        rowHeight: null,
+        onLayoutChange: function () {
+        },
+        compactType: null,
+        cols: {lg: 12, md: 12, sm: 6, xs: 3, xxs: 1},
+    };
+
+    state = {
+        currentBreakpoint: "lg",
+        mounted: false,
+    };
+
+    componentDidMount() {
+        this.setState({mounted: true});
+    }
+
+    onLayoutChange = (layout, layouts) => {
+        this.props.onLayoutChange(layout, layouts);
+        this.setState({layouts});
+    };
+
+    generateDOM() {
+        return this.props.children.map((child, index) => (
+            <div key={`${index}`}>
+                <div className="react-grid-item-border top"/>
+                <div className="react-grid-item-border left"/>
+                <div className="react-grid-item-border right"/>
+                <div className="react-grid-item-border bottom"/>
+                <div className="child-container">
+                    {child}
+                </div>
+            </div>
+        ))
+    }
+
+    render() {
+        return <>
+            <ResponsiveGridLayout
+                measureBeforeMount={false}
+                useCSSTransforms={this.state.mounted}
+                preventCollision={!this.props.compactType}
+                {...this.props}
+                onLayoutChange={this.onLayoutChange}>
+                {this.generateDOM()}
+            </ResponsiveGridLayout>
+        </>
+    }
+}
+
+
+const GridLayout = (
+    {
+        children,
+        render,
+        layouts: _layouts,
+        cols = {lg: 12, md: 12, sm: 12, xs: 12, xxs: 12},
+        ...props
+    }
+) => {
+    let layouts = _layouts
+    if (!_layouts) {
+        layouts = {}
+        const w = 2, h = 4;
+        Object.keys(cols).forEach((key) => {
+            const n = cols[key];
+            layouts[key] = children.map((element, i) => ({
+                i: `${i}`,
+                x: (i * w) % n,
+                y: Math.floor(i * w / n) * h,
+                w,
+                h,
+                minW: 0,
+                maxW: Infinity,
+                minH: 0,
+                maxH: Infinity,
+                // If true, equal to `isDraggable: false, isResizable: false`.
+                static: false,
+                // If false, will not be draggable. Overrides `static`.
+                isDraggable: true,
+                // If false, will not be resizable. Overrides `static`.
+                isResizable: true,
+                // By default, a handle is only shown on the bottom-right (southeast) corner.
+                // As of RGL >= 1.4.0, resizing on any corner works just fine!
+                resizeHandles: ['se'], // ['s', 'w' , 'e' , 'n' , 'sw' , 'nw' , 'se' , 'ne']
+                // If true and draggable, item will be moved only within grid.
+                isBounded: false
+            }))
+        })
+    }
+    return <div className={'grid-layout-container'}
+                style={{height: '100%', width: '100%', overflow: 'auto'}}>
+        <Layout draggableHandle={'.react-grid-item-border'}
+                layouts={layouts} {...props}>
+            {children}
+        </Layout>
+    </div>
+};
+export default GridLayout;
--- schedula/utils/form/react/src/core/components/index.js
+++ schedula/utils/form/react/src/core/components/index.js
@@ -6,6 +6,7 @@ const DiffViewer = React.lazy(() => import('./DiffViewer'));
 const Domain = React.lazy(() => import('./Domain'));
 const Element = React.lazy(() => import('./Element'));
 const FlexLayout = React.lazy(() => import('./FlexLayout'));
+const GridLayout = React.lazy(() => import('./GridLayout'));
 const Static = React.lazy(() => import('./Static'));
 const Stripe = React.lazy(() => import('./Stripe'));
 const Title = React.lazy(() => import('./Title'));
@@ -18,6 +19,7 @@ export function generateComponents() {
         Domain,
         Element,
         FlexLayout,
+        GridLayout,
         Static,
         Stripe,
         Title
