558d8229ad891c3772ee7168ac9948ed26b9c388
vincenzoarcidiacono <vinci1it2000@gmail.com>
2024-04-19 09:08:15 +0200

    feat(form): Remove `ExcelPreview` component and widget.

--- schedula/utils/form/react/src/core/widgets/index.js
+++ schedula/utils/form/react/src/core/widgets/index.js
@@ -1,10 +1,8 @@
-import ExcelPreviewWidget from './ExcelPreviewWidget'
 import ReCAPTCHAWidget from './ReCAPTCHA'
 import ValueWidget from './ValueWidget'
 
 export function generateWidgets() {
     return {
-        ExcelPreviewWidget,
         ReCAPTCHAWidget,
         ValueWidget
     }
