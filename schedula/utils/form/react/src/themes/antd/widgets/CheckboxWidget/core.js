9de67ccac5987271ee2bcac1abc9f9d0d195561f
vincenzoarcidiacono <vinci1it2000@gmail.com>
2024-05-28 11:29:58 +0200

    feat(form): Add options for `CheckboxWidget`.

--- schedula/utils/form/react/src/themes/antd/widgets/CheckboxWidget/core.js
+++ schedula/utils/form/react/src/themes/antd/widgets/CheckboxWidget/core.js
@@ -13,37 +13,34 @@ export default function CheckboxWidget(props) {
         onBlur,
         onChange,
         onFocus,
-        options,
+        options: {label: option_label = label, props: option_props, ...options},
         readonly,
         value,
     } = props;
     const {readonlyAsDisabled = true} = formContext;
 
-    const handleChange = ({target}) =>
-        onChange(target.checked);
+    const handleChange = ({target}) => onChange(target.checked);
 
-    const handleBlur = ({target}) =>
-        onBlur(id, target.checked);
+    const handleBlur = ({target}) => onBlur(id, target.checked);
 
-    const handleFocus = ({target}) =>
-        onFocus(id, target.checked);
+    const handleFocus = ({target}) => onFocus(id, target.checked);
 
     const extraProps = {
         onBlur: !readonly ? handleBlur : undefined,
         onFocus: !readonly ? handleFocus : undefined,
     };
 
-    return (
-        <Checkbox
-            autoFocus={autofocus}
-            checked={typeof value === "undefined" ? false : value}
-            disabled={disabled || (readonlyAsDisabled && readonly)}
-            id={id}
-            name={id}
-            onChange={!readonly ? handleChange : undefined}
-            {...extraProps}
-            aria-describedby={ariaDescribedByIds(id)}>
-            {labelValue(options.hasOwnProperty('label') ? options.label : label, hideLabel, '')}
-        </Checkbox>
-    );
+    return <Checkbox
+        autoFocus={autofocus}
+        checked={typeof value === "undefined" ? false : value}
+        disabled={disabled || (readonlyAsDisabled && readonly)}
+        id={id}
+        name={id}
+        onChange={!readonly ? handleChange : undefined}
+        {...extraProps}
+        {...options}
+        {...option_props}
+        aria-describedby={ariaDescribedByIds(id)}>
+        {labelValue(option_label, hideLabel, '')}
+    </Checkbox>
 }
