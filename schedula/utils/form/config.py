e482e0a1034e40278ec0daaea4c31707ebfdb636
vincenzoarcidiacono <vinci1it2000@gmail.com>
2024-08-08 11:52:55 +0200

    fix(form): Make all configs settable from envs.

--- schedula/utils/form/config.py
+++ schedula/utils/form/config.py
@@ -63,15 +63,30 @@ class Config:
         self.MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER')
         self.MAIL_SERVER = os.environ.get('MAIL_HOST')
         self.MAIL_PORT = int(os.environ.get('MAIL_PORT', 465))
-        self.MAIL_USE_SSL = True
+        self.MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL', 'true').lower() == 'true'
         self.MAIL_USERNAME = os.environ.get('MAIL_USER')
         self.MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-
-        # Use an in-memory db
-        self.SQLALCHEMY_DATABASE_URI = 'sqlite://'
+        self.DB_DIALECT = os.environ.get('DB_DIALECT', 'sqlite')
+        self.DB_USER = os.environ.get('DB_USER', '')
+        self.DB_PASSWORD = os.environ.get('DB_PASSWORD', '')
+        self.DB_HOST = os.environ.get('DB_HOST', '')
+        self.DB_PORT = os.environ.get('DB_PORT', '')
+        self.DB_DATABASE = os.environ.get('DB_DATABASE', 'database.db')
+        self.DB_SERVER = (
+                self.DB_USER and f'{self.DB_USER}:{self.DB_PASSWORD}@' or ''
+        )
+        self.DB_SERVER += (
+                self.DB_HOST and f'{self.DB_HOST}:{self.DB_PORT}' or ''
+        )
+        self.SQLALCHEMY_DATABASE_URI = os.environ.get(
+            'SQLALCHEMY_DATABASE_URI',
+            f'{self.DB_DIALECT}://{self.DB_SERVER}/{self.DB_DATABASE}'
+        )
         # As of Flask-SQLAlchemy 2.4.0 it is easy to pass in options directly to the
         # underlying engine. This option makes sure that DB connections from the
         # pool are still valid. Important for entire application since
         # many DBaaS options automatically close idle connections.
         self.SQLALCHEMY_ENGINE_OPTIONS = {"pool_pre_ping": True}
-        self.SQLALCHEMY_TRACK_MODIFICATIONS = False
+        self.SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get(
+            'SQLALCHEMY_TRACK_MODIFICATIONS', 'false'
+        ).lower() == 'true'
