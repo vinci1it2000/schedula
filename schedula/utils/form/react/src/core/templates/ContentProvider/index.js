b1b3a3981a6ff801d5e7a36e2ca5a288acc8689a
vincenzoarcidiacono <vinci1it2000@gmail.com>
2024-10-30 11:12:54 +0100

    feat(form): Add translator to plasmic components.

--- schedula/utils/form/react/src/core/templates/ContentProvider/index.js
+++ schedula/utils/form/react/src/core/templates/ContentProvider/index.js
@@ -1,6 +1,20 @@
 import React, {useEffect} from "react";
 import {PlasmicRootProvider} from '@plasmicapp/loader-react';
 import {usePlasmicStore} from "../../models/plasmic";
+import {Trans, useTranslation} from 'react-i18next';
+
+// Defined as a hook; should be used and passed as translator
+// prop to PlasmicRootProvider
+function usePlasmicTranslator() {
+    const {t} = useTranslation();
+    return (key, opts) => {
+        if (opts?.components) {
+            return <Trans i18nKey={key} components={opts.components}/>;
+        } else {
+            return t(key, {defaultValue: opts.message});
+        }
+    };
+}
 
 export default function ContentProvider(
     {
@@ -13,9 +27,11 @@ export default function ContentProvider(
     useEffect(() => {
         setPlasmicOpts(options);
     }, [options]);
-
+    const translator = usePlasmicTranslator()
     return PLASMIC ?
-        <PlasmicRootProvider loader={PLASMIC} {...props}>
+        <PlasmicRootProvider
+            loader={PLASMIC}
+            translator={translator} {...props}>
             {children}
         </PlasmicRootProvider> : children
 }
