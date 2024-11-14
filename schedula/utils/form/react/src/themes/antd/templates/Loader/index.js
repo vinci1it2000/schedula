51964bd0ccf6abe46ba6c73fcbec7a098cadcd46
vincenzoarcidiacono <vinci1it2000@gmail.com>
2024-11-14 12:50:16 +0100

    fix(form): Correct loader css.

--- schedula/utils/form/react/src/themes/antd/templates/Loader/index.js
+++ schedula/utils/form/react/src/themes/antd/templates/Loader/index.js
@@ -1,10 +1,12 @@
 import {Spin} from 'antd';
-
+import './index.css'
 const Loader = ({loading, children, ...props}) => {
-    return <Spin spinning={loading} size="large"
-                 style={{maxHeight: null}} {...props}>
-        {children}
-    </Spin>
+    return <div className={'main-spin'} style={{height: '100%', width: '100%'}}>
+        <Spin spinning={loading} size="large"
+              style={{maxHeight: null}} {...props}>
+            {children}
+        </Spin>
+    </div>
 
 };
 export default Loader;
