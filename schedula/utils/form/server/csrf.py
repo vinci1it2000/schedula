9230b78f821ca29ce204a84ea6ba6d0fd407025a
vincenzoarcidiacono <vinci1it2000@gmail.com>
2025-01-11 17:42:16 +0100

    doc(core): Update Copyright.

--- schedula/utils/form/server/csrf.py
+++ schedula/utils/form/server/csrf.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 # -*- coding: UTF-8 -*-
 #
-# Copyright 2015-2024, Vincenzo Arcidiacono;
+# Copyright 2015-2025, Vincenzo Arcidiacono;
 # Licensed under the EUPL (the 'Licence');
 # You may not use this work except in compliance with the Licence.
 # You may obtain a copy of the Licence at: http://ec.europa.eu/idabc/eupl
