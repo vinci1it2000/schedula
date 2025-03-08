fc05ed84b251ca0d1d6b8ace6e2fe437edc988f6
vincenzoarcidiacono <vinci1it2000@gmail.com>
2025-03-08 14:28:24 +0100

    feat(form): Update resources.

--- schedula/utils/form/sample/webpack.config.js
+++ schedula/utils/form/sample/webpack.config.js
@@ -372,7 +372,6 @@ module.exports = function (_env, argv) {
                                 }
                             });
                         });
-
                     });
                 }
             },
