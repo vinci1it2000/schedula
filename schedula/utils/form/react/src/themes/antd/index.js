ebe289919efa284715fc02f331750324a56bb2f4
vincenzoarcidiacono <vinci1it2000@gmail.com>
2025-10-26 09:04:52 +0100

    feat(form): Expose useLocaleStore ad form props.

--- schedula/utils/form/react/src/themes/antd/index.js
+++ schedula/utils/form/react/src/themes/antd/index.js
@@ -3,14 +3,15 @@ import Templates, {generateTemplates} from "./templates";
 import Widgets, {generateWidgets} from "./widgets";
 import Components, {generateComponents} from "./components";
 import notify from './notify'
-
+import {useLocaleStore} from "./models/locale";
 export function generateTheme(registerComponents = false) {
     return {
         templates: generateTemplates(),
         widgets: generateWidgets(),
         fields: generateFields(),
         components: generateComponents(registerComponents),
-        notify
+        notify,
+        useLocaleStore
     };
 }
 
