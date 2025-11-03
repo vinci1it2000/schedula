9a11554ff44243f368a3474b7e27af303e788394
vincenzoarcidiacono <vinci1it2000@gmail.com>
2025-11-03 18:56:41 +0100

    fix(form): Correct ReactDOM export.

--- schedula/utils/form/react/src/index.js
+++ schedula/utils/form/react/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import * as ReactRouterDOM from "react-router-dom";
 import * as plasmicLoaderReact from '@plasmicapp/loader-react'
 import renderForm, {
